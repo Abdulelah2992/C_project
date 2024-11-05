fd592feb142f0565e6ce8823e6b2db5b426c8a79
Abdulelah2992 <abdulelahemad1@gmail.com>
2024-11-05 12:38:53 +0300

    project

--- assembler.c
+++ assembler.c
@@ -1,48 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-#define N 100  
+#define N 100 
+#include <ctype.h>
 
 int k = 0, i = 0;
 char ArSpace[N], ArNoSpace[N];
 
 void compere(FILE *ptr) {
-    if (strcmp(ArNoSpace, "Ra=0") == 0) {
+    if (strcmp(ArNoSpace, "RA=0") == 0) {
         fprintf(ptr, "00001000\n");
-    } else if (strcmp(ArNoSpace, "Ra=1") == 0) {
+    } else if (strcmp(ArNoSpace, "RA=1") == 0) {
         fprintf(ptr, "00001001\n");
-    } else if (strcmp(ArNoSpace, "Ra=2") == 0) {
+    } else if (strcmp(ArNoSpace, "RA=2") == 0) {
         fprintf(ptr, "00001010\n");
-    }else if (strcmp(ArNoSpace, "Ra=3") == 0) {
+    }else if (strcmp(ArNoSpace, "RA=3") == 0) {
         fprintf(ptr, "00001011\n");
-    }else if (strcmp(ArNoSpace, "Ra=4") == 0) {
+    }else if (strcmp(ArNoSpace, "RA=4") == 0) {
         fprintf(ptr, "00001100\n");
-    }else if (strcmp(ArNoSpace, "Ra=5") == 0) {
+    }else if (strcmp(ArNoSpace, "RA=5") == 0) {
         fprintf(ptr, "00001101\n");
-    }else if (strcmp(ArNoSpace, "Ra=6") == 0) {
+    }else if (strcmp(ArNoSpace, "RA=6") == 0) {
         fprintf(ptr, "00001110\n");
-    }else if (strcmp(ArNoSpace, "Ra=7") == 0) {
+    }else if (strcmp(ArNoSpace, "RA=7") == 0) {
         fprintf(ptr, "00001111\n");
-    }else if (strcmp(ArNoSpace, "Rb=0") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=0") == 0) {
         fprintf(ptr, "00011000\n");
-    }else if (strcmp(ArNoSpace, "Rb=1") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=1") == 0) {
         fprintf(ptr, "00011001\n");
-    }else if (strcmp(ArNoSpace, "Rb=2") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=2") == 0) {
         fprintf(ptr, "00011010\n");
-    }else if (strcmp(ArNoSpace, "Rb=3") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=3") == 0) {
         fprintf(ptr, "00011011\n");
-    }else if (strcmp(ArNoSpace, "Rb=4") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=4") == 0) {
         fprintf(ptr, "00011100\n");
-    }else if (strcmp(ArNoSpace, "Rb=5") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=5") == 0) {
         fprintf(ptr, "00011101\n");
-    }else if (strcmp(ArNoSpace, "Rb=6") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=6") == 0) {
         fprintf(ptr, "00011110\n");
-    }else if (strcmp(ArNoSpace, "Rb=7") == 0) {
+    }else if (strcmp(ArNoSpace, "RB=7") == 0) {
         fprintf(ptr, "00011111\n");
-    } else if (strcmp(ArNoSpace, "Ro=Ra") == 0) {
+    } else if (strcmp(ArNoSpace, "RO=RA") == 0) {
         fprintf(ptr, "00100000\n");
-    } else if (strcmp(ArNoSpace, "Rb=Ra+Rb") == 0) {
+    } else if (strcmp(ArNoSpace, "RB=RA+RB") == 0) {
         fprintf(ptr, "00010000\n");
-    } else if (strcmp(ArNoSpace, "Rb=Ra-Rb") == 0) {
+    } else if (strcmp(ArNoSpace, "RB=RA-RB") == 0) {
         fprintf(ptr, "00010100\n");
     } else if (strcmp(ArNoSpace, "JC=0") == 0) {
         fprintf(ptr, "01110000\n");
@@ -60,9 +61,9 @@ void compere(FILE *ptr) {
         fprintf(ptr, "01110110\n");
     }else if (strcmp(ArNoSpace, "JC=7") == 0) {
         fprintf(ptr, "01110111\n");
-    } else if (strcmp(ArNoSpace, "Ra=Ra+Rb") == 0) {
+    } else if (strcmp(ArNoSpace, "RA=RA+RB") == 0) {
         fprintf(ptr, "00000000\n");
-    } else if (strcmp(ArNoSpace, "Ra=Ra-Rb") == 0) {
+    } else if (strcmp(ArNoSpace, "RA=RA-RB") == 0) {
         fprintf(ptr, "00000100\n");
     } else if (strcmp(ArNoSpace, "J=0") == 0) {
         fprintf(ptr, "10110000\n");
@@ -107,8 +108,9 @@ void check() {
     while (fgets(ArSpace, N, fp)) {
         k = 0;
         for (i = 0; ArSpace[i] != '\0'; i++) {
-            if (ArSpace[i] != ' ' && ArSpace[i] != '\t') {
-                ArNoSpace[k++] = ArSpace[i];
+            if (ArSpace[i] != ' ' && ArSpace[i] != '\t' && ArSpace[i] != '\n') {
+                   
+		    ArNoSpace[k++] =toupper(ArSpace[i]);
             }
         }
         ArNoSpace[k] = '\0';
